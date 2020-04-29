679aafc75b18c234f565b6469093a0adaa720cfe
Ummaromana Sama <ummaromanasama@gmail.com>
2020-04-29 02:43:07 -0400

    Add files via upload

--- colorMaskWordCloud.py
+++ colorMaskWordCloud.py
@@ -19,18 +19,27 @@ def get_wiki(search):
 #Function data to varible
 data = get_wiki(search)
 
-#Batman mask for word cloud
+#Batman mask
 batman_mask = np.array(Image.open(requests.get('https://www.freepnglogos.com/uploads/yellow-black-batman-logo-png-2.png', stream=True).raw))
 
+#Spongebob mask
+spongebob_mask = np.array(Image.open(requests.get('https://pngimg.com/uploads/spongebob/spongebob_PNG11.png', stream=True).raw))
+
+#Marvel mask
+marvel_mask = np.array(Image.open(requests.get('https://logos-download.com/wp-content/uploads/2018/07/Marvel_logo_red.png', stream=True).raw))
+
+#Simpsons mask
+simpsons_mask = np.array(Image.open(requests.get('https://pngimg.com/uploads/simpsons/simpsons_PNG63.png', stream=True).raw))
+
 #Word cloud format
-word_cloud = WordCloud(width = 300, height = 300, background_color='white', stopwords=STOPWORDS, mask=batman_mask).generate(data)
+word_cloud = WordCloud(width = 300, height = 300, background_color='white', stopwords=STOPWORDS, mask=simpsons_mask).generate(data)
 
 #Creating coloring from image
-image_colors = ImageColorGenerator(batman_mask)
+image_colors = ImageColorGenerator(simpsons_mask)
 plt.figure(figsize=[7,7])
 plt.imshow(word_cloud.recolor(color_func=image_colors), interpolation="bilinear")
 plt.axis("off")
 _=plt.show()
   
 #Generating wordcloud
-generate_wordcloud(data, batman_mask)
+generate_wordcloud(data, simpsons_mask)
