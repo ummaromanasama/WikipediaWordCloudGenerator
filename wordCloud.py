a8a93296e054153f560bc3ecb1bc660e52b6afb6
Ummaromana Sama <ummaromanasama@gmail.com>
2020-05-30 00:17:53 -0400

    Update wordCloud.py

--- wordCloud.py
+++ wordCloud.py
@@ -1,4 +1,4 @@
-#Importing Libraries
+#Import modules
 from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 from PIL import Image
 import urllib
@@ -7,39 +7,38 @@ import numpy as np
 import matplotlib.pyplot as plt
 import wikipedia 
 
-#Request user input
+#Prompt for search input
 search = input('Search something on Wikipedia: ')
 
-#Wikipedia search
+#Retriveve Wikipedia page
 def get_wiki(search):
     title = wikipedia.search(search)[0]
     page = wikipedia.page(title)
     return page.content
 
-#Function data to varible
+#Store page data
 data = get_wiki(search)
 
-#Sample mask links
-#Marvel mask: https://logos-download.com/wp-content/uploads/2018/07/Marvel_logo_red.png
-#Mickey mask: https://i.pinimg.com/originals/22/92/c8/2292c8f0bf0c2e166c3c08e6adf2f014.png
-#Penny mask: https://cdn.getstickerpack.com/storage/uploads/sticker-pack/penny-wise/sticker_7.png?71e44a420b56adf1f8e34b49b2374bf0
-#Puppy mask: https://pngimg.com/uploads/dog/dog_PNG50375.png
-#Pikachu mask: https://www.pngmart.com/files/2/Pikachu-PNG-Transparent-Image.png
+'''
+Sample mask links
+Marvel mask: https://logos-download.com/wp-content/uploads/2018/07/Marvel_logo_red.png
+Mickey mask: https://i.pinimg.com/originals/22/92/c8/2292c8f0bf0c2e166c3c08e6adf2f014.png
+Penny mask: https://cdn.getstickerpack.com/storage/uploads/sticker-pack/penny-wise/sticker_7.png?71e44a420b56adf1f8e34b49b2374bf0
+Puppy mask: https://pngimg.com/uploads/dog/dog_PNG50375.png
+Pikachu mask: https://www.pngmart.com/files/2/Pikachu-PNG-Transparent-Image.png
+'''
 
-#Change mask 
-change_mask = 'https://logos-download.com/wp-content/uploads/2018/07/Marvel_logo_red.png'
-offical_mask = np.array(Image.open(requests.get(change_mask, stream=True).raw))
+#Upload image 
+mask_link = 'insert image address'
+offical_mask = np.array(Image.open(requests.get(mask_link, stream=True).raw))
 
-#Background color
+#Format word cloud
 back_color = '#ffffff'
-
-#Word cloud format
 word_cloud = WordCloud(width = 300, height = 300, background_color=back_color, stopwords=STOPWORDS, mask=offical_mask).generate(data)
-
-#Creating coloring from image
 image_colors = ImageColorGenerator(offical_mask)
+
+#Pop-up format
 plt.figure(figsize=[7,7])
 plt.imshow(word_cloud.recolor(color_func=image_colors), interpolation="bilinear")
 plt.axis("off")
 _=plt.show()
-  
